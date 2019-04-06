e42f950144c59273406d81c47c05a3a727083121
D057235 <D057235@WDFN34381060A.emea.global.corp.sap>
2019-04-06 10:05:30 +0200

    Tambah jenis pelayanan "Musik SM"

--- src/main/java/org/persekutuankarlsruhe/webapp/sheets/Pelayanan.java
+++ src/main/java/org/persekutuankarlsruhe/webapp/sheets/Pelayanan.java
@@ -1,27 +1,27 @@
 package org.persekutuankarlsruhe.webapp.sheets;
 
 public enum Pelayanan {
-    PEMIMPIN_RENUNGAN("Pemimpin Renungan"), MC("MC"), MUSIK("Musik"), PEMUSIK("Pemusik"), SM("Sekolah Minggu"), MASAK("Masak"), USHER(
-            "Usher/Kolektan"), AV("AV"), PA_ANAK_BESAR("PA anak Kelas Besar"), PA_ANAK_KECIL(
-                    "PA anak Kelas Kecil"), RINGKASAN_KHOTBAH("Ringkasan Kotbah"), KONSUMSI(
-                            "Konsumsi (Akhir Bulan)"), MC_BIRTHDAY("MC Birthday (akhir bulan)");
+	PEMIMPIN_RENUNGAN("Pemimpin Renungan"), MC("MC"), MUSIK("Musik"), PEMUSIK("Pemusik"), SM("Sekolah Minggu"),
+	MASAK("Masak"), USHER("Usher/Kolektan"), AV("AV"), PA_ANAK_BESAR("PA anak Kelas Besar"),
+	PA_ANAK_KECIL("PA anak Kelas Kecil"), RINGKASAN_KHOTBAH("Ringkasan Kotbah"), KONSUMSI("Konsumsi (Akhir Bulan)"),
+	MC_BIRTHDAY("MC Birthday (akhir bulan)"), MUSIK_SM("Musik SM");
 
-    String nama;
+	String nama;
 
-    private Pelayanan(String nama) {
-        this.nama = nama;
-    }
+	private Pelayanan(String nama) {
+		this.nama = nama;
+	}
 
-    public static Pelayanan fromName(String nama) {
-        for (Pelayanan pelayanan : Pelayanan.values()) {
-            if (pelayanan.nama.equalsIgnoreCase(nama)) {
-                return pelayanan;
-            }
-        }
-        return null;
-    }
+	public static Pelayanan fromName(String nama) {
+		for (Pelayanan pelayanan : Pelayanan.values()) {
+			if (pelayanan.nama.equalsIgnoreCase(nama)) {
+				return pelayanan;
+			}
+		}
+		return null;
+	}
 
-    public String getNama() {
-        return nama;
-    }
+	public String getNama() {
+		return nama;
+	}
 }
