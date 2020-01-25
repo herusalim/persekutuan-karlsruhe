3ba77473d20aa92f579ad19a819cffa4c49b6a6f
Heru Martinus Salim <heru.martinus.salim@sap.com>
2020-01-25 18:33:00 +0100

    Bugfix: more logs

--- src/main/java/org/persekutuankarlsruhe/webapp/sheets/JadwalPelayanan.java
+++ src/main/java/org/persekutuankarlsruhe/webapp/sheets/JadwalPelayanan.java
@@ -1,183 +1,188 @@
 package org.persekutuankarlsruhe.webapp.sheets;
 
+import org.persekutuankarlsruhe.webapp.calendar.CalendarUtil;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-
-import org.persekutuankarlsruhe.webapp.calendar.CalendarUtil;
 
 public class JadwalPelayanan implements Serializable {
 
-	private static final int DEFAULT_JAM_MULAI_PERSEKUTUAN = 10;
+    private static final int DEFAULT_JAM_MULAI_PERSEKUTUAN = 10;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4526458381387539869L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 4526458381387539869L;
 
-	/**
-	 * Instructs date, time, datetime, and duration fields to be output as
-	 * doubles in "serial number" format, as popularized by Lotus 1-2-3. Days
-	 * are counted from December 31st 1899 and are incremented by 1, and times
-	 * are fractions of a day. For example, January 1st 1900 at noon would be
-	 * 1.5, 1 because it's 1 day offset from December 31st 1899, and .5 because
-	 * noon is half a day. February 1st 1900 at 3pm would be 32.625. This
-	 * correctly treats the year 1900 as not a leap year.
-	 */
-	private static final Calendar KALENDER_REFERENSI;
-	static {
-		KALENDER_REFERENSI = Calendar.getInstance(CalendarUtil.getTimeZone());
-		KALENDER_REFERENSI.set(1899, 11, 31);
-	}
+    /**
+     * Instructs date, time, datetime, and duration fields to be output as
+     * doubles in "serial number" format, as popularized by Lotus 1-2-3. Days
+     * are counted from December 31st 1899 and are incremented by 1, and times
+     * are fractions of a day. For example, January 1st 1900 at noon would be
+     * 1.5, 1 because it's 1 day offset from December 31st 1899, and .5 because
+     * noon is half a day. February 1st 1900 at 3pm would be 32.625. This
+     * correctly treats the year 1900 as not a leap year.
+     */
+    private static final Calendar KALENDER_REFERENSI;
 
-	private Date tanggal;
-	private Map<Pelayanan, List<Orang>> daftarPelayanan = new HashMap<Pelayanan, List<Orang>>();
-	private String bahanRenungan;
-	private String lokasi;
+    static {
+        KALENDER_REFERENSI = Calendar.getInstance(CalendarUtil.getTimeZone());
+        KALENDER_REFERENSI.set(1899, 11, 31);
+    }
 
-	/**
-	 * Constructor JadwalPelayanan
-	 * 
-	 * @param tanggal
-	 *            Tanggal persekutuan (see
-	 *            {@link JadwalPelayanan#KALENDER_REFERENSI})
-	 */
-	public JadwalPelayanan(BigDecimal tanggal) {
-		Calendar kalender = (Calendar) KALENDER_REFERENSI.clone();
-		kalender.add(Calendar.DAY_OF_MONTH, tanggal.intValue() - 1);
-		setJamPersekutuan(kalender);
-		this.tanggal = kalender.getTime();
-	}
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
 
-	private void setJamPersekutuan(Calendar kalender) {
-		kalender.set(Calendar.HOUR_OF_DAY, DEFAULT_JAM_MULAI_PERSEKUTUAN);
-		kalender.set(Calendar.MINUTE, 0);
-		kalender.set(Calendar.SECOND, 0);
-		kalender.set(Calendar.MILLISECOND, 0);
-	}
+    private Date tanggal;
+    private Map<Pelayanan, List<Orang>> daftarPelayanan = new HashMap<Pelayanan, List<Orang>>();
+    private String bahanRenungan;
+    private String lokasi;
 
-	/**
-	 * Constructor dengan tanggal dengan format serial number
-	 * 
-	 * @param tanggal
-	 *            Tanggal persekutuan
-	 * 
-	 */
-	public JadwalPelayanan(Date tanggal) {
-		this.tanggal = tanggal;
-	}
+    /**
+     * Constructor JadwalPelayanan
+     *
+     * @param tanggal Tanggal persekutuan (see
+     *                {@link JadwalPelayanan#KALENDER_REFERENSI})
+     */
+    public JadwalPelayanan(BigDecimal tanggal) {
+        Calendar kalender = (Calendar) KALENDER_REFERENSI.clone();
+        kalender.add(Calendar.DAY_OF_MONTH, tanggal.intValue() - 1);
+        setKalender(kalender);
+    }
 
-	/**
-	 * Menambahkan petugas ke daftar pelayanan
-	 * 
-	 * @param pelayanan
-	 *            Jenis pelayanan
-	 * @param petugas
-	 *            petugas
-	 */
-	public void tambahPetugas(Pelayanan pelayanan, Orang petugas) {
-		List<Orang> daftarPetugas = daftarPelayanan.get(pelayanan);
-		if (daftarPetugas == null) {
-			daftarPetugas = new ArrayList<Orang>();
-		}
-		daftarPetugas.add(petugas);
-		daftarPelayanan.put(pelayanan, daftarPetugas);
-	}
+    public JadwalPelayanan(String tanggal) {
+        Calendar kalender = Calendar.getInstance();
+        try {
+            kalender.setTime(DATE_FORMAT.parse(tanggal));// all done
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Invalid tanggal: " + tanggal, e);
+        }
+        setKalender(kalender);
+    }
 
-	/**
-	 * Mengambil daftar petugas dari pelayanan tertentu
-	 * 
-	 * @param pelayanan
-	 *            Jenis pelayanan
-	 * @return List dari petugas, atau <code>null</code> kalau tidak ditemukan
-	 */
-	public List<Orang> getPetugas(Pelayanan pelayanan) {
-		return daftarPelayanan.get(pelayanan);
-	}
+    private void setKalender(Calendar kalender) {
+        setJamPersekutuan(kalender);
+        this.tanggal = kalender.getTime();
+    }
 
-	/**
-	 * @return semua daftar pelayanan
-	 */
-	public Set<Entry<Pelayanan, List<Orang>>> listPelayanan() {
-		return daftarPelayanan.entrySet();
-	}
+    private void setJamPersekutuan(Calendar kalender) {
+        kalender.set(Calendar.HOUR_OF_DAY, DEFAULT_JAM_MULAI_PERSEKUTUAN);
+        kalender.set(Calendar.MINUTE, 0);
+        kalender.set(Calendar.SECOND, 0);
+        kalender.set(Calendar.MILLISECOND, 0);
+    }
 
-	public Date getTanggal() {
-		return this.tanggal;
-	}
+    /**
+     * Constructor dengan tanggal dengan format serial number
+     *
+     * @param tanggal Tanggal persekutuan
+     */
+    public JadwalPelayanan(Date tanggal) {
+        this.tanggal = tanggal;
+    }
 
-	/**
-	 * Memberikan nama bahan renungan
-	 * 
-	 * @param bahanRenungan
-	 *            Tema/bahan renungan
-	 */
-	public void setBahanRenungan(String bahanRenungan) {
-		this.bahanRenungan = bahanRenungan;
-	}
+    /**
+     * Menambahkan petugas ke daftar pelayanan
+     *
+     * @param pelayanan Jenis pelayanan
+     * @param petugas   petugas
+     */
+    public void tambahPetugas(Pelayanan pelayanan, Orang petugas) {
+        List<Orang> daftarPetugas = daftarPelayanan.get(pelayanan);
+        if (daftarPetugas == null) {
+            daftarPetugas = new ArrayList<Orang>();
+        }
+        daftarPetugas.add(petugas);
+        daftarPelayanan.put(pelayanan, daftarPetugas);
+    }
 
-	/**
-	 * @return Bahan renungan
-	 */
-	public String getBahanRenungan() {
-		return this.bahanRenungan;
-	}
+    /**
+     * Mengambil daftar petugas dari pelayanan tertentu
+     *
+     * @param pelayanan Jenis pelayanan
+     * @return List dari petugas, atau <code>null</code> kalau tidak ditemukan
+     */
+    public List<Orang> getPetugas(Pelayanan pelayanan) {
+        return daftarPelayanan.get(pelayanan);
+    }
 
-	public Map<Pelayanan, List<Orang>> getDaftarPelayanan() {
-		return this.daftarPelayanan;
-	}
+    /**
+     * @return semua daftar pelayanan
+     */
+    public Set<Entry<Pelayanan, List<Orang>>> listPelayanan() {
+        return daftarPelayanan.entrySet();
+    }
 
-	public String toString() {
-		String daftarPelayananString = "Pelayanan: ";
-		for (Entry<Pelayanan, List<Orang>> entry : daftarPelayanan.entrySet()) {
-			Pelayanan pelayanan = entry.getKey();
-			daftarPelayananString += (pelayanan == null ? "" : pelayanan.getNama()) + " [";
-			int index = 0;
-			for (Orang orang : entry.getValue()) {
-				if (index++ > 0) {
-					daftarPelayananString += ", ";
-				}
-				daftarPelayananString += orang.getNama();
-			}
-			daftarPelayananString += "]; ";
-		}
-		return new SimpleDateFormat().format(tanggal) + "\t" + "Bahan Renungan: " + bahanRenungan + "\t"
-				+ daftarPelayananString + "Lokasi: " + getLokasi();
-	}
+    public Date getTanggal() {
+        return this.tanggal;
+    }
 
-	public List<Orang> getMc() {
-		return getPetugas(Pelayanan.MC);
-	}
+    /**
+     * Memberikan nama bahan renungan
+     *
+     * @param bahanRenungan Tema/bahan renungan
+     */
+    public void setBahanRenungan(String bahanRenungan) {
+        this.bahanRenungan = bahanRenungan;
+    }
 
-	public List<Orang> getPemimpinRenungan() {
-		return getPetugas(Pelayanan.PEMIMPIN_RENUNGAN);
-	}
+    /**
+     * @return Bahan renungan
+     */
+    public String getBahanRenungan() {
+        return this.bahanRenungan;
+    }
 
-	public List<Orang> getSekolahMinggu() {
-		return getPetugas(Pelayanan.SM);
-	}
+    public Map<Pelayanan, List<Orang>> getDaftarPelayanan() {
+        return this.daftarPelayanan;
+    }
 
-	public List<Orang> getMusik() {
-		return getPetugas(Pelayanan.MUSIK);
-	}
+    public String toString() {
+        String daftarPelayananString = "Pelayanan: ";
+        for (Entry<Pelayanan, List<Orang>> entry : daftarPelayanan.entrySet()) {
+            Pelayanan pelayanan = entry.getKey();
+            daftarPelayananString += (pelayanan == null ? "" : pelayanan.getNama()) + " [";
+            int index = 0;
+            for (Orang orang : entry.getValue()) {
+                if (index++ > 0) {
+                    daftarPelayananString += ", ";
+                }
+                daftarPelayananString += orang.getNama();
+            }
+            daftarPelayananString += "]; ";
+        }
+        return new SimpleDateFormat().format(tanggal) + "\t" + "Bahan Renungan: " + bahanRenungan + "\t"
+                + daftarPelayananString + "Lokasi: " + getLokasi();
+    }
 
-	public List<Orang> getMasak() {
-		return getPetugas(Pelayanan.MASAK);
-	}
+    public List<Orang> getMc() {
+        return getPetugas(Pelayanan.MC);
+    }
 
-	public String getLokasi() {
-		return this.lokasi;
-	}
+    public List<Orang> getPemimpinRenungan() {
+        return getPetugas(Pelayanan.PEMIMPIN_RENUNGAN);
+    }
 
-	public void setLokasi(String lokasi) {
-		this.lokasi = lokasi;
-	}
+    public List<Orang> getSekolahMinggu() {
+        return getPetugas(Pelayanan.SM);
+    }
+
+    public List<Orang> getMusik() {
+        return getPetugas(Pelayanan.MUSIK);
+    }
+
+    public List<Orang> getMasak() {
+        return getPetugas(Pelayanan.MASAK);
+    }
+
+    public String getLokasi() {
+        return this.lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
 }
